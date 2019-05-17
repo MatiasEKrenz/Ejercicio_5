bb2ed9cf70c60af8264cf8730517b90058c8503e
MatiasEKrenz <matiaskrenz@gmail.com>
2019-05-17 14:56:50 -0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var router = express.Router();
+const https = require('https');
+
+
+
+router.get('/', function(req, res, next) {
+
+    https.get('https://api.mercadolibre.com/sites', (response) => {
+
+        var info = '';
+        response.on('data', (d) => {
+            //process.stdout.write(d);
+            info += d;
+        });
+
+        response.on('end', () => {
+            res.render('index', { sites: JSON.parse(info) });
+        });
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
+
+
+  //res.render('index');
+});
+
+router.post('/trends', function(req, res, next) {
+
+
+    var pais = req.body.pais;
+    var categoria = req.body.categoria;
+    const columns = parseInt(req.body.columnas);
+    const rows = parseInt(req.body.filas);
+
+    var finalUrl = "https://api.mercadolibre.com/trends/" + pais;
+
+    if (categoria != "") {
+        finalUrl += "/" + categoria;
+    }
+
+    https.get(finalUrl, (response) => {
+
+        var info = '';
+        response.on('data', (d) => {
+            //process.stdout.write(d);
+            info += d;
+        });
+
+        /*response.on('end', () => {
+            res.render('trendsss', { trendss: JSON.parse(info) });
+        });
+*/
+
+
+
+        response.on('end', () => {
+            var trendsData = JSON.parse(info);
+            var trendsArray = new Array(rows);
+            for(var i = 0; i < trendsArray.length; i++) {
+                trendsArray[i] = trendsData.slice(i*rows, i*rows+columns);
+            }
+            res.render('trendsss', { trends: trendsArray });
+        });
+
+
+
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
+
+    //process.stdout.write(texto);
+    //res.render('trend', { title: texto });
+});
+
+module.exports = router;
+
+
+/*
+        //for cla in trendss
+            //p #{cla.keyword}
+ */
